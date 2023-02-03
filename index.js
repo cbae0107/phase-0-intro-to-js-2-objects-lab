74c059bb006aafa232cc495ad6c5e699e2bf311a
Changhyun <chang.bae@flatironschool.com>
2023-02-02 22:53:25 -0800

    commit tree 225604e463cbd0779ace8856493ba39585314d7d

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// Write your solution in this file!
+const employee = {
+    name: "Sam",
+    streetAddress: "11 Broadway"
+}
+function updateEmployeeWithKeyAndValue(employee, key, value){
+    const employeeA = {...employee};
+    employeeA[key] = value;
+    return employeeA;
+}
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value){
+    employee[key] = value;
+    return employee;
+}
+function deleteFromEmployeeByKey(employee, key){
+    const employeeA = {...employee};
+    delete employeeA[key];
+    return employeeA;
+}
+function destructivelyDeleteFromEmployeeByKey(employee, key){
+    delete employee[key];
+    return employee;
+}
